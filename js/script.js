1801b63e8885f63b6f4aa6823135bfb6f6463cea
Tom <66076646+TomMaverick@users.noreply.github.com>
2024-10-14 15:46:57 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector('.toggle-button').addEventListener('click', function() {
+    document.body.classList.toggle('light-mode');
+
+    // Update icon based on current mode
+    document.querySelector('.toggle-button').innerHTML = document.body.classList.contains('light-mode')
+        ? '<i class="fas fa-moon"></i>'  // Moon icon for light mode
+        : '<i class="fas fa-sun"></i>';   // Sun icon for dark mode
+});
